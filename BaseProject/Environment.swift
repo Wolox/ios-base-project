8e30f609940d352fed4f1cc35e0c6a8030480e4c
Pablo Giorgi <pablo.giorgi@wolox.com.ar>
2017-05-18 14:37:56 -0300

    Set environment for test and UI test and Environment properties

--- BaseProject/Environment.swift
+++ BaseProject/Environment.swift
@@ -8,13 +8,16 @@
 
 import Foundation
 
-enum Environment {
+internal enum Environment {
     
     case development
     case alpha
     case beta
     case production
-    case test
+    
+}
+
+internal extension Environment {
     
     static var current: Environment {
         #if DEVELOPMENT
@@ -30,15 +33,33 @@ enum Environment {
             return .production
         #endif
     }
+
+}
+
+internal extension Environment {
+    
+    private static let EnvironmentKey = "ENVIRONMENT"
+    
+    // This property should be set in scheme Test action's arguments (environment variables).
+    private static let EnvironmentTest = "Test"
+    
+    // This property should be set in UITests by launchEnvironment arguments.
+    private static let EnvironmentUITest = "UITest"
     
     static var isTestTarget: Bool {
-        if case .test = Environment.current {
-            return true
-        } else {
-            return false
-        }
+        let environment = ProcessInfo.processInfo.environment
+        return environment[Environment.EnvironmentKey] == Environment.EnvironmentTest
     }
     
+    static var isUITestTarget: Bool {
+        let environment = ProcessInfo.processInfo.environment
+        return environment[Environment.EnvironmentKey] == Environment.EnvironmentUITest
+    }
+    
+}
+
+internal extension Environment {
+
     static var isRunningOnSimulator: Bool {
         #if (arch(i386) || arch(x86_64)) && os(iOS)
             return true
--- BaseProjectUITests/BaseProjectUITests.swift
+++ BaseProjectUITests/BaseProjectUITests.swift
@@ -19,7 +19,7 @@ class BaseProjectUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let application = XCUIApplication()
-        application.launchEnvironment = ["ENVIRONMENT": "Test"]
+        application.launchEnvironment = ["ENVIRONMENT": "UITest"]
         application.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
