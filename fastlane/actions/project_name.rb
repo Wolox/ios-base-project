b85ba8e14a95c8fcbca1f33934727119904f68af
Pablo Giorgi <pablo.giorgi@wolox.com.ar>
2017-10-12 20:24:26 -0300

    Infer the project name from the project
    Create a new action that infers the project name from the project, as long as there is a target that matches the project name.
    This action allows to be provided a project_name in case the inferred name should not be used.
    Taking advantage of this action, actions that inferred the project name by their own, now extend this new one, delegating this behavior.
    Also, properties from .env.default have been moved to Matchfile, since they have to be there in case match is run from terminal.

---
+++ fastlane/actions/project_name.rb
@@ -0,0 +1,71 @@
+require 'xcodeproj'
+
+module Fastlane
+  module Actions
+    class ProjectNameAction < Action
+
+      PROJECT_EXTENSION = ".xcodeproj".freeze
+
+      # This script returns the application name
+      # if there is a scheme that matches the project name.
+      # Otherwise, it assumes the project name should be specified 
+      # by the user and fails.
+
+      def self.run(params)
+        default_project_name
+      end
+
+      # Fastlane Action class required functions.
+
+      def self.is_supported?(platform)
+        true
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :project_name, optional: true, default_value: default_project_name)
+        ]
+      end
+
+      # Available options default_value helpers
+
+      # In case there is a single '.xcodeproj' in the default directory
+      # it can be automatically inferred by the script 
+      # if no parameter project is received.
+      def self.default_project
+        projects = Dir.entries('.').select { |each| File.extname(each) == PROJECT_EXTENSION }
+        if projects.length == 0
+          UI.abort_with_message! "No projects with extension %s found in root directory." % PROJECT_EXTENSION
+        end
+        if projects.length > 1
+          UI.abort_with_message! "Multiple projects with extension %s found in root directory." % PROJECT_EXTENSION
+        end
+        projects.first
+      end
+
+      # In case there is a scheme matching project's name
+      # it can be automatically inferred by the script 
+      # if no parameter scheme is received.
+      def self.matching_scheme
+        target = Xcodeproj::Project.open(default_project)
+          .targets
+          .find { |each| each.name == File.basename(default_project, File.extname(default_project)) }
+        if target.nil?
+          UI.abort_with_message! "No target matching project name %s found." % PROJECT_EXTENSION
+        end
+        target.name
+      end
+
+      # Just a wrapper for the matching scheme function.
+      def self.default_project_name
+        matching_scheme
+      end
+
+      # Name of the project file.
+      def self.default_project_filename
+        matching_scheme + PROJECT_EXTENSION
+      end
+
+    end
+  end
+end
